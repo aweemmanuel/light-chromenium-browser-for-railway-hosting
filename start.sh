9d0c5416e9fc0b44588376f6c0af60d0b77e2fe3
aweemmanuel <117767329+aweemmanuel@users.noreply.github.com>
2026-07-06 02:51:35 +0100

    Add start.sh to launch Xvfb and xpra with Chromium

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export DISPLAY=:100
+
+Xvfb :100 -screen 0 1366x768x24 &
+
+sleep 2
+
+xpra start :100 \
+    --bind-tcp=0.0.0.0:${PORT} \
+    --html=on \
+    --daemon=no \
+    --exit-with-children \
+    --start-child="chromium \
+        --no-sandbox \
+        --disable-dev-shm-usage \
+        --disable-gpu \
+        --disable-sync \
+        --disable-extensions \
+        --disable-background-networking \
+        --no-first-run \
+        --renderer-process-limit=1 \
+        about:blank"
