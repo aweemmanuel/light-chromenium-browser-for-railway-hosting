7b4df5a52bb4eedd5d4c9803a23873099e378244
aweemmanuel <117767329+aweemmanuel@users.noreply.github.com>
2026-07-06 03:02:50 +0100

    Enhance start.sh with error handling and directory setup
    Updated start.sh to include error handling and create necessary directories.

--- start.sh
+++ start.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
+set -e
 
-export DISPLAY=:100
+export PORT=${PORT:-8080}
 
-Xvfb :100 -screen 0 1366x768x24 &
+mkdir -p /tmp/chrome
+mkdir -p /run/dbus
 
-sleep 2
+# Remove stale X lock files in case of an unclean shutdown
+rm -f /tmp/.X100-lock
+rm -rf /tmp/.X11-unix/X100
 
-xpra start :100 \
+exec xpra start :100 \
     --bind-tcp=0.0.0.0:${PORT} \
     --html=on \
     --daemon=no \
+    --mdns=no \
+    --notifications=no \
+    --pulseaudio=no \
+    --printing=no \
+    --webcam=no \
+    --speaker=off \
+    --microphone=off \
+    --exit-with-children=no \
     --start-child="chromium \
         --no-sandbox \
+        --disable-setuid-sandbox \
         --disable-dev-shm-usage \
         --disable-gpu \
+        --disable-software-rasterizer \
+        --disable-background-networking \
         --disable-sync \
         --disable-extensions \
-        --disable-background-networking \
+        --disable-component-update \
+        --disable-default-apps \
+        --disable-translate \
         --no-first-run \
-        --renderer-process-limit=1 \
+        --no-default-browser-check \
+        --user-data-dir=/tmp/chrome \
         about:blank"
